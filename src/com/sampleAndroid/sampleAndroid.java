0ffe6da623ae0cc88ab00ea11546baa613130bef
tnanek <tnanek@gmail.com>
2013-06-07 21:20:24 -0400

    Test Android App, using Sublime Text 2.

---
+++ src/com/sampleAndroid/sampleAndroid.java
@@ -0,0 +1,15 @@
+package com.sampleAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class sampleAndroid extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
